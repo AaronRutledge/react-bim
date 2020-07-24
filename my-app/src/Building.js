d0fb1ab4a8c4a4a437fb90e3d85e5d99f8e61567
Aaron Rutledge <aaronhrutledge@gmail.com>
2020-07-23 21:55:27 -0600

    Wiring up XML to form

--- my-app/src/Building.js
+++ my-app/src/Building.js
@@ -1,5 +1,4 @@
 import React from "react"
-import ReactDomServer from "react-dom/server"
 import { times } from 'lodash'
 
 const Building = (props) => React.createElement("Building", props)
@@ -16,54 +15,161 @@ const Floors = (props) => React.createElement("Floors", props)
 const ComponentName = (props) => React.createElement("ComponentName", props)
 const BoundaryPoints = (props) => React.createElement("BoundaryPoints", props)
 const Point = (props) => React.createElement("Point", props)
+const StartPoint = (props) => React.createElement("StartPoint", props)
+const EndPoint = (props) => React.createElement("EndPoint", props)
+const Walls = (props) => React.createElement("Walls", props)
 const X = (props) => React.createElement("X", props)
 const Y = (props) => React.createElement("Y", props)
 const Z = (props) => React.createElement("Z", props)
-const StartPoint = (props) => React.createElement("StartPoint", props)
 const Height = (props) => React.createElement("Height", props)
 
 
-const formData = {
-    "floors": 4,
-    "floorHeight": 100,
-    "buildingWidth": 200,
-    "buildingHeight": 200,
-    "roomsPerFloor": 4
-}
+// const formData = {
+//     "floors": 6,
+//     "floorHeight": 12,
+//     "buildingWidth": 200,
+//     "buildingLength": 300,
+//     "roomsPerFloor": 4
+// }
+
 
 const ConfiguredBuilding = (props) => {
-    let floors = [];
-    let i = 0;
-    times(formData.floors, () => {
-        floors.push(
-            <Floor>
-                <ComponentName>Generic8inFloor</ComponentName>
-                <BoundaryPoints>
-                    <Point>
-                        <X>0</X>
-                        <Y>{i * formData.floorHeight}</Y>
-                        <Z>0</Z>
-                    </Point>
-                    <Point>
-                        <X>{formData.buildingWidth}</X>
-                        <Y>{i * formData.floorHeight}</Y>
-                        <Z>0</Z>
-                    </Point>
-                    <Point>
-                        <X>{formData.buildingWidth}</X>
-                        <Y>{i * formData.floorHeight}</Y>
-                        <Z>{formData.buildingHeight}</Z>
-                    </Point>
-                    <Point>
-                        <X>0</X>
-                        <Y>{i * formData.floorHeight}</Y>
-                        <Z>{formData.buildingHeight}</Z>
-                    </Point>
-                </BoundaryPoints>
-            </Floor>
-        )
-        i++
-    })
+    const formData = props.formData
+
+    const renderFLoors = () => {
+        let floors = [];
+        let i = 0;
+        times(formData.floors, () => {
+            floors.push(
+                <Floor>
+                    <ComponentName>Generic8inFloor</ComponentName>
+                    <BoundaryPoints>
+                        <Point>
+                            <X>0</X>
+                            <Y>0</Y>
+                            <Z>{i * formData.floorHeight}</Z>
+                        </Point>
+                        <Point>
+                            <X>0</X>
+                            <Y>{formData.buildingWidth}</Y>
+                            <Z>{i * formData.floorHeight}</Z>
+                        </Point>
+                        <Point>
+                            <X>{formData.buildingLength}</X>
+                            <Y>{formData.buildingWidth}</Y>
+                            <Z>{i * formData.floorHeight}</Z>
+                        </Point>
+                        <Point>
+                            <X>{formData.buildingLength}</X>
+                            <Y>0</Y>
+                            <Z>{i * formData.floorHeight}</Z>
+                        </Point>
+                    </BoundaryPoints>
+                </Floor>
+            )
+            i++
+        })
+        return floors;
+    }
+    
+    const renderWalls = () => {
+        let walls = [];
+        let i = 0;
+        times(formData.floors, () => {
+            walls.push(
+                <React.Fragment>
+                    <Wall>
+                        <ComponentName>Generic8inCmuWall</ComponentName>
+                        <StartPoint>
+                            <X>0</X>
+                            <Y>0</Y>
+                            <Z>{i * formData.floorHeight}</Z>
+                        </StartPoint>
+                        <EndPoint>
+                            <X>0</X>
+                            <Y>{formData.buildingWidth}</Y>
+                            <Z>{i * formData.floorHeight}</Z>
+                        </EndPoint>
+                        <Height>{formData.floorHeight}</Height>
+                    </Wall>
+                    <Wall>
+                        <ComponentName>Generic8inCmuWall</ComponentName>
+                        <StartPoint>
+                            <X>0</X>
+                            <Y>{formData.buildingWidth}</Y>
+                            <Z>{i * formData.floorHeight}</Z>
+                        </StartPoint>
+                        <EndPoint>
+                            <X>{formData.buildingLength}</X>
+                            <Y>{formData.buildingWidth}</Y>
+                            <Z>{i * formData.floorHeight}</Z>
+                        </EndPoint>
+                        <Height>{formData.floorHeight}</Height>
+                    </Wall>
+                    <Wall>
+                        <ComponentName>Generic8inCmuWall</ComponentName>
+                        <StartPoint>
+                            <X>{formData.buildingLength}</X>
+                            <Y>{formData.buildingLength}</Y>
+                            <Z>{i * formData.floorHeight}</Z>
+                        </StartPoint>
+                        <EndPoint>
+                            <X>{formData.buildingLength}</X>
+                            <Y>0</Y>
+                            <Z>{i * formData.floorHeight}</Z>
+                        </EndPoint>
+                        <Height>{formData.floorHeight}</Height>
+                    </Wall>
+                    <Wall>
+                        <ComponentName>Generic8inCmuWall</ComponentName>
+                        <StartPoint>
+                            <X>{formData.buildingLength}</X>
+                            <Y>0</Y>
+                            <Z>{i * formData.floorHeight}</Z>
+                        </StartPoint>
+                        <EndPoint>
+                            <X>0</X>
+                            <Y>0</Y>
+                            <Z>{i * formData.floorHeight}</Z>
+                        </EndPoint>
+                        <Height>{formData.floorHeight}</Height>
+                    </Wall>
+                    {renderInteriorWallsForFloor(formData.roomsPerFloor, i * formData.floorHeight)}
+                </React.Fragment>
+            )
+            i++
+        })
+        return walls;
+    }
+    
+    
+    const renderInteriorWallsForFloor = (roomCount, elevation) => {
+        let interiorWalls = [];
+        let i = 1;
+        const numberOfInterioirWalls = roomCount - 1
+        const roomSpacing = formData.buildingLength / roomCount
+        times(numberOfInterioirWalls, () => {
+            interiorWalls.push(
+                <Wall>
+                    <ComponentName>Generic6inStudWall</ComponentName>
+                    <StartPoint>
+                        <X>{roomSpacing * i}</X>
+                        <Y>0</Y>
+                        <Z>{elevation}</Z>
+                    </StartPoint>
+                    <EndPoint>
+                        <X>{roomSpacing * i}</X>
+                        <Y>{formData.buildingWidth}</Y>
+                        <Z>{elevation}</Z>
+                    </EndPoint>
+                    <Height>{formData.floorHeight}</Height>
+                </Wall>
+            )
+            i++
+        })
+        return interiorWalls;
+    }    
+
     return (
         <Building>
             <ProjectInformation>
@@ -72,12 +178,15 @@ const ConfiguredBuilding = (props) => {
                 </Units>
             </ProjectInformation>
             <Floors>
-                {floors}
+                {renderFLoors()}
             </Floors>
-
+            <Walls>
+                {renderWalls()}
+            </Walls>
         </Building>
     )
 }
 
+export default ConfiguredBuilding
 
-export const buildingXML = ReactDomServer.renderToStaticMarkup(<ConfiguredBuilding />)
+//export const buildingXML = ReactDomServer.renderToStaticMarkup(<ConfiguredBuilding />)
--- my-app/src/Form.js
+++ my-app/src/Form.js
@@ -1,13 +1,15 @@
 // Helper styles for demo
 import "./helper.css";
 import { DisplayFormikState } from "./helper";
-import {buildingXML} from "./Building";
+//import {buildingXML} from "./Building";
+import ConfiguredBuilding from "./Building";
 
 import React from "react";
+import ReactDomServer from "react-dom/server";
 import { Formik } from "formik";
 import * as Yup from "yup";
 
-console.log("here is some XML", buildingXML);
+//console.log("here is some XML", buildingXML);
 
 const Form = () => (
   <div className="app">
@@ -22,6 +24,7 @@ const Form = () => (
       onSubmit={async values => {
         await new Promise(resolve => setTimeout(resolve, 500));
         alert(JSON.stringify(values, null, 2));
+        alert(ReactDomServer.renderToStaticMarkup(<ConfiguredBuilding formData={values}/>))
       }}
       validationSchema={Yup.object().shape({
         floors: Yup.number()
@@ -29,17 +32,11 @@ const Form = () => (
           .min(1)
           .max(50),
         floorHeight: Yup.number()
-          .required("Required")
-          .min(2000)
-          .max(5000),
+          .required("Required"),
         buildingWidth: Yup.number()
-          .required("Required")
-          .min(2000)
-          .max(50000),
+          .required("Required"),
         buildingLength: Yup.number()
-          .required("Required")
-          .min(2000)
-          .max(50000),
+          .required("Required"),
         roomsPerFloor: Yup.number()
           .required("Required")
           .min(1)
